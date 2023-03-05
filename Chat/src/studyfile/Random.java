eb95c6d00e10c86a62bd8a7d6526b7142ce79b73
dasol <dasol@localhost>
2023-03-05 17:32:50 +0900

    random 추

--- Chat/src/studyfile/Random.java
+++ Chat/src/studyfile/Random.java
@@ -7,7 +7,7 @@ public class Random {
 		int random = (int)(Math.random()*6+1);
 		int number = random;
 		System.out.println("주사위 번호 : "+ number );
-		
+		System.out.println("주사위 번호 : "+ number );		
 
 	}
 
